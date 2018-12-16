fd7c6c660aa16239e05bc5cac9516faa8bfebb25
k1.tehrani@gmail.com <BlackHeart59>
2018-12-16 23:01:33 +0330

    Master Rule implemented

---
+++ src/main/java/com/hexad/MasterRule.java
@@ -0,0 +1,24 @@
+package com.hexad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by k1 on 12/16/18.
+ * email:<k1.tehrani@gmail.com>
+ */
+public class MasterRule implements RuleAware {
+    private final RuleAware[] rules;
+
+    public MasterRule(RuleAware[] rules) {
+        this.rules = rules;
+    }
+
+    public List<Action> process(GoodType good) {
+        List<Action> actions = new ArrayList<Action>();
+        for (RuleAware rule : rules) {
+            actions.addAll(rule.process(good));
+        }
+        return actions;
+    }
+}
