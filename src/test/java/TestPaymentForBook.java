c7f4b278dc8546cdf41b1ff5665eaca7c3bc8139
Kayvan Hajhassantehrani <k1.tehrani@gmail.com>
2018-12-17 17:43:08 +0330

    test against list items completed

--- src/test/java/TestPaymentForBook.java
+++ src/test/java/TestPaymentForBook.java
@@ -1,7 +1,13 @@
+import static junit.framework.TestCase.assertTrue;
+
 import com.hexad.model.Agent;
 import com.hexad.model.Book;
 import com.hexad.model.Payment;
+import com.hexad.service.Action;
 import com.hexad.service.MasterRule;
+import com.hexad.service.actions.GenerateCommissionAction;
+import com.hexad.service.actions.PackageSlipAction;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,10 +17,14 @@ import org.junit.Test;
  */
 public class TestPaymentForBook {
 
-    @Test
-    public void testActionsCount() {
-        Payment payment = new Payment(new Book(new Agent("Jack")));
-        MasterRule masterRule = new MasterRule();
-        Assert.assertEquals(masterRule.process(payment.getPaidForGood()).size(), 3);
-    }
+  @Test
+  public void testActionsCount() {
+    Payment payment = new Payment(new Book(new Agent("Jack")));
+    MasterRule masterRule = new MasterRule();
+    List<Action> actions = masterRule.process(payment.getPaidForGood());
+    Assert.assertEquals(actions.size(), 3);
+    assertTrue(actions.stream().anyMatch(e -> e instanceof PackageSlipAction));
+    assertTrue(actions.stream().anyMatch(e -> e instanceof GenerateCommissionAction));
+    Assert.assertEquals(actions.stream().filter(e -> e instanceof PackageSlipAction).count(), 2);
+  }
 }
